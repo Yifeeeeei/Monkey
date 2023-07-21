d90b5656abd697101ba6af83d6bbc82fef734de2
if <55980773+Yifeeeeei@users.noreply.github.com>
2023-07-21 23:32:07 +0800

    Update csdn_free_copy.user.js

--- csdn_free_copy.user.js
+++ csdn_free_copy.user.js
@@ -1,16 +1,28 @@
 // ==UserScript==
 // @name         csdn_free_copy
-// @version      0.1
+// @version      0.2
 // @description  copy code block from csdn without login
 // @author       Yifeeeeei
 // @updateURL    https://yifeeeeei.github.io/Monkey/csdn_free_copy.user.js
 // @downloadURL  https://yifeeeeei.github.io/Monkey/csdn_free_copy.user.js
-// @grant        GM_addStyle
 // @include      *csdn.net*
 // ==/UserScript==
 
 (function () {
-    const style_free_copy =
-        "#content_views pre code{-webkit-user-select: text !important; user-select: text !important;}  #content_views pre{-webkit-user-select: text !important; user-select: text !important;}";
-    GM_addStyle(style_free_copy);
+    window.oncontextmenu = document.oncontextmenu = document.oncopy = null;
+    [...document.querySelectorAll("body")].forEach(
+        (dom) => (dom.outerHTML = dom.outerHTML)
+    );
+    [...document.querySelectorAll("body, body *")].forEach((dom) => {
+        [
+            "onselect",
+            "onselectstart",
+            "onselectend",
+            "ondragstart",
+            "ondragend",
+            "oncontextmenu",
+            "oncopy",
+        ].forEach((ev) => dom.removeAttribute(ev));
+        dom.style["user-select"] = "auto";
+    });
 })();
