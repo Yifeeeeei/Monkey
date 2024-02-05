7d020dc829c769e02dfe8cc2c477ebe553a80037
if <55980773+Yifeeeeei@users.noreply.github.com>
2024-02-04 16:53:42 -0800

    Update colorful_day.user.js

--- colorful_day.user.js
+++ colorful_day.user.js
@@ -10,6 +10,7 @@
 // @include      *baidu.com*
 // @include      *qq.com*
 // @include      *jd.com*
+// @include      *e-hentai.org*
 // ==/UserScript==
 
 var domain = window.location.href;
@@ -27,6 +28,24 @@ if (domain.indexOf("bilibili.com") != -1) {
     const style =
         "html{-webkit-filter: grayscale(.0) !important;    filter: grayscale(0%) !important;}";
     GM_addStyle(style);
+} else if (domain.indexOf("e-hentai.org") != -1) {
+    const style =
+        "#img{\
+        position: fixed;\
+        max-width: 95% !important;\
+        max-height: 95% !important;\
+        transform: translate(-50%, -50%);\
+        top: 50%;\
+        left: 50%;\
+        z-index: 9999;\
+        object-fit: contain !important;}";
+    var img_ele = document.getElementById("img");
+
+    GM_addStyle(style);
+    if (img_ele) {
+    } else {
+        console.log("img not found");
+    }
 } else if (domain.indexOf("jd.com") != -1) {
     const baidu_body = document.getElementsByTagName("html")[0];
     var body_class = baidu_body.classList;
